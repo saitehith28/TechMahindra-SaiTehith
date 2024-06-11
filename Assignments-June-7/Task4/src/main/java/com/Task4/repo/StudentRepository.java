863ea9d4c72fdcb31d01fce3210ba0a5fbf85652
Tehith <103926873+saitehith28@users.noreply.github.com>
2024-06-12 00:01:14 +0530

    Delete Assignments-June-7/Task4 directory

--- Assignments-June-7/Task4/src/main/java/com/Task4/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Task4.repo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.Task4.model.Student;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Integer> {
-	
-}
