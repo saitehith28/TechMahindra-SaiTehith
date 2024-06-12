f0cd7ba1ce10eb1bc546bbb52ad3c97a9693c43e
Tehith <103926873+saitehith28@users.noreply.github.com>
2024-06-13 01:15:03 +0530

    Add files via upload

---
+++ Assignments4-June-7/Task7/src/main/java/com/animals/Color.java
@@ -0,0 +1,25 @@
+package com.animals;
+
+public class Color {
+	private String baseColor;
+	private String textureColor;
+	
+	public String getBaseColor() {
+		return baseColor;
+	}
+	public void setBaseColor(String baseColor) {
+		this.baseColor=baseColor;
+	}
+	
+	public String getTextureColor() {
+		return textureColor;
+	}
+	public void setTextureColor(String textureColor) {
+		this.textureColor=textureColor;
+	}
+	
+	@Override
+	public String toString() {
+		return baseColor + " base skin color " + textureColor + " textureColor";
+	}
+}
