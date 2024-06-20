d25bfb1b82e7e0a496afc4a4be3c03a9d8026193
Tehith <103926873+saitehith28@users.noreply.github.com>
2024-06-21 00:34:24 +0530

    Delete Javascript Tasks directory

--- Javascript Tasks/Task1.js
+++
@@ -1,3 +0,0 @@
-var farenheit=parseInt(prompt("Enter the Temperature in farenheit"))
-var celsius=(farenheit-32)*5/9
-document.write("The Celcius Temperature is: " + celsius);
--- Javascript Tasks/Task2.js
+++
@@ -1,6 +0,0 @@
-var num=parseInt(prompt("Enter the Number"))
-var result=1;
-for(let i=1;i<=num;i++){
-    result=result*i;
-}
-document.write("The factorial of " + num + " is " + result);
